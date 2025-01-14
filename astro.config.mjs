1752f76489357ee9c81184dba941e89666ed729e
emre-akakce <emre@akakce.com>
2025-01-14 13:26:16 +0300

    use SSR (not static)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 // @ts-check
 import react from '@astrojs/react';
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()]
+    integrations: [react()],
+    output: 'server',
+    adapter: node({
+        mode: 'standalone', // Use 'standalone' for a single server file
+      }),
 });
